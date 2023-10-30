db05dbd8654cf1df9006b078cfa523ce1a12bbaf
Jeffrey Pisa <jeffrey.pisa@gmail.com>
2023-10-30 10:51:44 +0100

    Fix

--- lib/timber.php
+++ lib/timber.php
@@ -17,12 +17,10 @@ Timber::$dirname = array('templates', 'views');
 class StarterSite extends TimberSite {
 
 	function __construct() {
-		add_theme_support( 'post-formats' );
-		add_theme_support( 'post-thumbnails' );
 		add_theme_support( 'menus' );
 		add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
-		add_filter( 'timber_context', array( $this, 'add_to_context' ) );
-		add_filter( 'get_twig', array( $this, 'add_to_twig' ) );
+		add_action( 'after_setup_theme', array( $this, 'theme_supports' ) );
+		add_filter( 'timber/context', array( $this, 'add_to_context' ) );
 		add_action( 'init', array( $this, 'register_post_types' ) );
 		add_action( 'init', array( $this, 'register_taxonomies' ) );
 		parent::__construct();
@@ -41,23 +39,16 @@ class StarterSite extends TimberSite {
 		$context['footermenu'] = new TimberMenu('footermenu');
 		$context['overlaymenu'] = new TimberMenu('overlaymenu');
 		$context['site'] = $this;
-    $context['lang'] = strtolower( substr( get_locale(), 0, 2 ));
-    if ( wp_is_mobile() ) {
-    	$context['ismobile'] = 'true';
-    }
-    return $context;
+		$context['lang'] = strtolower( substr( get_locale(), 0, 2 ));
+		if ( wp_is_mobile() ) {
+			$context['ismobile'] = 'true';
+		}
+	return $context;
 	}
 
 	function myfoo( $text ) {
 		$text .= ' bar!';
 		return $text;
-	}
-
-	function add_to_twig( $twig ) {
-		/* this is where you can add your own functions to twig */
-		$twig->addExtension( new Twig_Extension_StringLoader() );
-		$twig->addFilter('myfoo', new Twig_SimpleFilter('myfoo', array($this, 'myfoo')));
-		return $twig;
 	}
 
 }
